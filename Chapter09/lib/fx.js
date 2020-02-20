475c72a81d2969f99619dbea7eaf60ae545ead36
DevRappers <devrappers@gmail.com>
2020-02-20 12:00:08 +0900

    Chapter8 복습 및 Chapter9(동시성 프로그래밍 2) 학습

--- Chapter09/lib/fx.js
+++ Chapter09/lib/fx.js
@@ -7,18 +7,21 @@ const isIterable = a => a && a[Symbol.iterator];
 
 const go1 = (a, f) => a instanceof Promise ? a.then(f) : f(a);
 
+const reduceF = (acc, a, f) =>
+    a instanceof Promise ?
+        a.then(a => f(acc, a), e => e == nop ? acc : Promise.reject(e)) :
+        f(acc, a);
+
+const head = iter => go1(take(1, iter), ([h]) => h);
+
 const reduce = curry((f, acc, iter) => {
-    if (!iter) {
-        iter = acc[Symbol.iterator]();
-        acc = iter.next().value;
-    } else {
-        iter = iter[Symbol.iterator]();
-    }
+    if (!iter) return reduce(f, head(iter = acc[Symbol.iterator]()), iter);
+
+    iter = iter[Symbol.iterator]();
     return go1(acc, function recur(acc) {
         let cur;
         while (!(cur = iter.next()).done) {
-            const a = cur.value;
-            acc = f(acc, a);
+            acc = reduceF(acc, cur.value, f);
             if (acc instanceof Promise) return acc.then(recur);
         }
         return acc;
@@ -32,13 +35,20 @@ const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
 const take = curry((l, iter) => {
     let res = [];
     iter = iter[Symbol.iterator]();
-    let cur;
-    while (!(cur = iter.next()).done) {
-        const a = cur.value;
-        res.push(a);
-        if (res.length == l) return res;
-    }
-    return res;
+    return function recur() {
+        let cur;
+        while (!(cur = iter.next()).done) {
+            const a = cur.value;
+            if (a instanceof Promise) {
+                return a
+                    .then(a => (res.push(a), res).length == l ? res : recur())
+                    .catch(e => e == nop ? recur() : Promise.reject(e));
+            }
+            res.push(a);
+            if (res.length == l) return res;
+        }
+        return res;
+    }();
 });
 
 const takeAll = take(Infinity);
@@ -52,13 +62,17 @@ L.range = function* (l) {
 
 L.map = curry(function* (f, iter) {
     for (const a of iter) {
-        yield f(a);
+        yield go1(a, f);
     }
 });
 
+const nop = Symbol('nop');
+
 L.filter = curry(function* (f, iter) {
     for (const a of iter) {
-        if (f(a)) yield a;
+        const b = go1(a, f);
+        if (b instanceof Promise) yield b.then(b => b ? a : Promise.reject(nop));
+        else if (b) yield a;
     }
 });
 
